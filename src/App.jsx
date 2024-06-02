f3b18d3ae7ac367885df461538a6e082bb85458d
quan1506 <mrquan1506@gmail.com>
2024-06-02 22:26:58 +0700

    Use hash based routing to levarage Github page deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Route, Switch, Redirect } from "wouter";
+import { Router, Route, Switch, Redirect } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import TemplatesSelectionPage from "./pages/templates-selection-page";
 import TemplateBuilderPage from "./pages/template-builder-page";
 import { PagePath } from "./config";
 
 const App = () => (
-  <Switch>
-    <Route path={PagePath.TEMPLATES} component={TemplatesSelectionPage} />
-    <Route
-      path={`${PagePath.BUILDER}/:templateId`}
-      component={TemplateBuilderPage}
-    />
-    <Route>
-      <Redirect to={PagePath.TEMPLATES} />
-    </Route>
-  </Switch>
+  <Router hook={useHashLocation}>
+    <Switch>
+      <Route path={PagePath.TEMPLATES} component={TemplatesSelectionPage} />
+      <Route
+        path={`${PagePath.BUILDER}/:templateId`}
+        component={TemplateBuilderPage}
+      />
+      <Route>
+        <Redirect to={PagePath.TEMPLATES} />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
