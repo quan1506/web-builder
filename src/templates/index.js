3704d5a48b996ae7ca897f8ba2f9f2fbc9f9a0ba
quan1506 <mrquan1506@gmail.com>
2024-06-02 21:00:32 +0700

    Replace correct name for hero banner template

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,9 +1,9 @@
-import template_hero_banner from "./template_hero_banner.json";
+import template_simple_article from "./template_simple_article.json";
 import template_newsletter from "./template_newsletter.json";
 import template_triple_product_list from "./template_triple_product_list.json";
 
 export const TEMPLATES = [
-  template_hero_banner,
+  template_simple_article,
   template_newsletter,
   template_triple_product_list,
 ];
